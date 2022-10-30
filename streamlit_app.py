76db5997459d3857006b13a3710b2d7f92c745b8
Jawadflahi <117039650+Jawadflahi@users.noreply.github.com>
2022-10-31 01:13:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ slt.title('My Parents new healthy diner')
 slt.heading('Breakfast Menu')
 slt.text('Omaga 3 and Blueberry Meal')
 slt.text('Kale,Spinach & Rocket Smoothie')
-slt.text('Hard boiled Free-Range Egg');
+slt.text('Hard boiled Free-Range Egg')
